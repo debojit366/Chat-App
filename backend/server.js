e278dc47251d9fe824ac6b3bf4223c428a48eb8d
debojit366 <debojitdas366@gmail.com>
2026-06-05 12:20:01 +0530

    feat: add allowed origins for CORS and update API base URL configuration

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,11 @@ const PORT = process.env.PORT || 5000;
 // 1. CORS Configuration
 app.use(cors({ origin: "*" })); 
 
+
+const allowedOrigins = [
+  'https://chat-app-theta-ten-58.vercel.app', 
+  'http://localhost:3000'                      
+];
 // 2. Body Parser
 app.use(express.json());
 
